7f2719a463efbe330f4152c2b3f660d758920147
JGW Maxwell <jgwmaxwell@gmail.com>
2011-01-06 04:19:53 +0000

    hashing

--- lib/api_auth/tokenize.rb
+++ lib/api_auth/tokenize.rb
@@ -1,11 +1,24 @@
+require 'diigest/sha2'
+
 module ApiAuth	
 	class Tokenize
+	
+		API_KEY = 'x34itcbhxijkhbilcbxi2lb5ixbx4ilubgcx'
+		API_SECRET = 'kijejwcnfli3rjng2jkrnxcli3khbjrgckj4ncx3l4kjwekjbf'
+	
 		def initialize(options={})
-			@tokenize_this = options[:tokenize_this]
+			@api_key = options[:api_key]
+			@api_secret = options[:api_secret]
 		end
 		
 		def generate_token
-			@tokenize_this + ActiveSupport::SecureRandom.base64(44).tr('+/=', 'xyz')
+			raw = @api_secret
+			hash = Digest::SHA2.new << raw
+			#@tokenize_this + ActiveSupport::SecureRandom.base64(44).tr('+/=', 'xyz')
+		end
+		
+		def validate_token
+			
 		end
 	end
 end
--- lib/api_auth/version.rb
+++ lib/api_auth/version.rb
@@ -1,3 +1,3 @@
 module ApiAuth
-  VERSION = "0.0.2.4"
+  VERSION = "0.0.2.5"
 end
